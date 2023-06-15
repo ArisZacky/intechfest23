aa40e055e147e87b7c0fc9c6c9931d1737f96292
Suryanandana <suryanandana85@gmail.com>
2023-06-15 10:43:54 +0800

    membuat halaman lomba jika blum daftar lomba

--- resources/views/peserta/content/profil.blade.php
+++ resources/views/peserta/content/profil.blade.php
@@ -59,7 +59,7 @@
                                     <div
                                         class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary-lightblue sm:max-w-md">
                                         <input type="text" name="username" id="username" autocomplete="username"
-                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                             placeholder="Masukkan alamat...">
                                     </div>
                                 </div>
@@ -71,7 +71,7 @@
                                     <div
                                         class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary-lightblue sm:max-w-md">
                                         <input type="text" name="username" id="username" autocomplete="username"
-                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                             placeholder="Masukkan nama instansi...">
                                     </div>
                                 </div>
@@ -83,7 +83,7 @@
                                     <div
                                         class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-primary-lightblue sm:max-w-md">
                                         <input type="text" name="username" id="username" autocomplete="username"
-                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            class="block flex-1 border-0 bg-transparent py-1.5 pl-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                                             placeholder="Masukkan nomer handphone...">
                                     </div>
                                 </div>
